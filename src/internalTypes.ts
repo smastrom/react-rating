48c132cc09b283a873f006c1343b4ba5a0ef4c03
smastrom <78644132+smastrom@users.noreply.github.com>
2022-10-16 21:48:20 +0200

    feat: add reset focus

--- src/internalTypes.ts
+++ src/internalTypes.ts
@@ -59,3 +59,4 @@ export type NonNullProp<K extends keyof RatingProps> = NonNullable<RatingProps[`
 export type HTMLProps = React.HTMLProps<HTMLDivElement>;
 export type MouseEvent = React.MouseEvent<HTMLDivElement>;
 export type FocusEvent = React.FocusEvent<HTMLDivElement>;
+export type MutableRef = React.MutableRefObject<HTMLDivElement | null>;

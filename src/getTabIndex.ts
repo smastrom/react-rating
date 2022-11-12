6fda84d0b4f7267ba5dc2aa1709d8975cc70970e
“s-mastrom” <60471784+smastrom1@users.noreply.github.com>
2022-11-12 22:02:27 +0100

    feat: moved reset btn to last child

--- src/getTabIndex.ts
+++ src/getTabIndex.ts
@@ -1,8 +1,18 @@
 import { TabIndex } from './internalTypes';
 
-export function getTabIndex(items: number, selectedIndex: number): TabIndex[] {
+export function getTabIndex(
+	items: number,
+	selectedIndex: number,
+	hasReset = false
+): TabIndex[] {
 	return Array.from({ length: items }, (_, index) => {
-		if (selectedIndex === -1 || selectedIndex === 0) {
+		if (hasReset && selectedIndex < 0) {
+			if (index === items - 1) {
+				return 0;
+			}
+			return -1;
+		}
+		if (selectedIndex <= 0) {
 			if (index === 0) {
 				return 0;
 			}
--- vite/App.tsx
+++ vite/App.tsx
@@ -1,24 +1,8 @@
 import { useState } from 'react';
 import { Profiler } from './Profiler';
 import { Rating } from '../src/Rating';
-import { ItemStyles, RatingProps } from '../src/exportedTypes';
-import { StickerStar } from '../src';
 
-const customStyles: ItemStyles = {
-	itemShapes: StickerStar,
-	activeFillColor: ['#FEE2E2', '#FFEDD5', '#FEF9C3', '#ECFCCB', '#D1FAE5'],
-	activeBoxColor: ['#da1600', '#db711a', '#dcb000', '#61bb00', '#009664'],
-	activeBoxBorderColor: ['#c41400', '#d05e00', '#cca300', '#498d00', '#00724c'],
-	inactiveFillColor: 'white',
-	inactiveBoxColor: '#dddddd',
-	inactiveBoxBorderColor: '#a8a8a8',
-};
-
-const CUSTOM_GROUP_LABEL = 'Rate - GitHub Plaza Hotel';
 const CUSTOM_GROUP_LABEL_ID = 'group_label';
-
-/* const CUSTOM_LABELS = ['Bad', 'Poor', 'Average', 'Very Good', 'Excellent'];
-const CUSTOM_LABELS_IDS = ['label_1', 'label_2', 'label_3', 'label_4', 'label_5']; */
 
 type Props = { isRequired?: boolean; initialRating?: number };
 
@@ -54,7 +38,6 @@ export function App({ isRequired = false, initialRating = 0 }: Props) {
 					isRequired={isRequired}
 					onChange={setRating}
 					value={rating}
-					// itemStyles={customStyles}
 					// highlightOnlySelected
 					// orientation="horizontal"
 					// radius="small"
